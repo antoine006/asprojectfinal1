b13c915533a2545b4be1ce9dba918ec26379fafb
antoine006 <antoineshukha@gmail.com>
2018-11-12 12:48:25 +0200

    fragments and scrollview

--- app/src/main/java/com/example/hp1/asproject/MainActivity.java
+++ app/src/main/java/com/example/hp1/asproject/MainActivity.java
@@ -2,21 +2,37 @@ package com.example.hp1.asproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<Item> arrayList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public static class CustomListActivity extends AppCompatActivity {
+        LinearLayout gallery = findViewById(R.id.gallery);
+        LayoutInflater inflater = LayoutInflater.from(this);
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_custom_list);
+        arrayList.add(new Item(R.drawable.action, "Action"));
+        arrayList.add(new Item(R.drawable.biography, "Biorgraphy"));
+
+        for(int i = 0; i< arrayList.size() ; i++){
+            View view = inflater.inflate(R.layout.horiz_item, gallery, false);
+            TextView text = view.findViewById(R.id.horText);
+            text.setText(arrayList.get(i).getName());
+
+            ImageView imageView = view.findViewById(R.id.imageView);
+            imageView.setImageResource(arrayList.get(i).getImage());
+
+            gallery.addView(view);
         }
     }
 }
--- app/src/main/java/com/example/hp1/asproject/NavigationDrawerActivity.java
+++ app/src/main/java/com/example/hp1/asproject/NavigationDrawerActivity.java
@@ -1,6 +1,5 @@
 package com.example.hp1.asproject;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -25,23 +24,20 @@ public class NavigationDrawerActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        if(savedInstanceState == null) {
+            NavigationView navigationView1= (NavigationView) findViewById(R.id.nav_view);
+            navigationView1.setNavigationItemSelectedListener(this);
+
+        }
     }
 
     @Override
@@ -82,14 +78,14 @@ public class NavigationDrawerActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.Sign_up) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new SignupFragment()).commit();
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.Sign_in) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.Cart) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.Top_10) {
 
 
         }
