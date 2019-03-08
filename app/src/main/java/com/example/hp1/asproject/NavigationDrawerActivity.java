df7ddbbb4d0a16aac8cdadb3c1b900184a072e5d
antoine <antoineshukha@gmail.com>
2019-03-08 09:51:33 +0200

    "camera"

--- app/src/main/java/com/example/hp1/asproject/NavigationDrawerActivity.java
+++ app/src/main/java/com/example/hp1/asproject/NavigationDrawerActivity.java
@@ -2,7 +2,9 @@ package com.example.hp1.asproject;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
@@ -15,11 +17,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import static com.example.hp1.asproject.CameraGalleryActivity.CAMERA_REQUEST;
 
 public class NavigationDrawerActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawer;
+    ImageView profilepicture;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,16 @@ public class NavigationDrawerActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        View i = navigationView.getHeaderView(0);
+        profilepicture = i.findViewById(R.id.profilepicture);
+
+        profilepicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(i,CAMERA_REQUEST);
+            }
+        });
 //        if (savedInstanceState == null) {
 //            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
 //                    new FragmentMain()).commit();
